b900d3da4e24352dd9445b59c3b54fcdac94be6c
Geovanny <geofh2321@gmail,com>
2021-04-29 23:07:14 -0600

    Archivos de Migracion

---
+++ DAL/Migrations/202104300452254_seedsConfiguration.cs
@@ -0,0 +1,63 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class seedsConfiguration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Roles",
+                c => new
+                    {
+                        IdRol = c.Int(nullable: false, identity: true),
+                        NombreRol = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdRol);
+            
+            CreateTable(
+                "dbo.Usuarios",
+                c => new
+                    {
+                        idUser = c.Int(nullable: false, identity: true),
+                        UserName = c.String(),
+                        email = c.String(),
+                        pass = c.String(),
+                        roleId = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.idUser)
+                .ForeignKey("dbo.Roles", t => t.roleId, cascadeDelete: true)
+                .Index(t => t.roleId);
+            
+            CreateTable(
+                "dbo.UserInfoes",
+                c => new
+                    {
+                        IdUserInformation = c.Int(nullable: false, identity: true),
+                        NombreUsuario = c.String(),
+                        ApellidoUsuario = c.String(),
+                        direccion = c.String(),
+                        FechaNacimiento = c.String(),
+                        telefono = c.String(),
+                        documento = c.String(),
+                        idUser = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.IdUserInformation)
+                .ForeignKey("dbo.Usuarios", t => t.idUser, cascadeDelete: true)
+                .Index(t => t.idUser);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Usuarios", "roleId", "dbo.Roles");
+            DropForeignKey("dbo.UserInfoes", "idUser", "dbo.Usuarios");
+            DropIndex("dbo.UserInfoes", new[] { "idUser" });
+            DropIndex("dbo.Usuarios", new[] { "roleId" });
+            DropTable("dbo.UserInfoes");
+            DropTable("dbo.Usuarios");
+            DropTable("dbo.Roles");
+        }
+    }
+}
---
+++ DAL/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+﻿namespace DAL.Migrations
+{
+    using DAL.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.Models.ComprasContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DAL.Models.ComprasContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            var role = new List<Role>()
+            {
+                new Role{NombreRol = rol.admin},
+                new Role{NombreRol = rol.user}
+            };
+
+            role.ForEach(r => context.roles.AddOrUpdate(rl => rl.NombreRol, r));
+            context.SaveChanges();
+        }
+    }
+}
